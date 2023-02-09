8e7be577c3e4ce17add4a0a9bdf92b4a6c5af9ad
tugi <blueqwinn@gmail.com>
2023-02-09 20:31:38 +0300

    first commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class Anagram
+    
+listen = Anagram.new("Listen")
+
+listen.match(%w[enlists google inlets banana])
+# => ["inlets"]
+
+
+def initialize('Diaper')
+diaper = Anagram.new('Diaper')
+end
+
+def initialize('ba')
+ba = Anagram.new('ba')
+end
+
+
+def initialize('listen')
+listen = Anagram.new('listen')
+end
+
+def initialize('allergy')
+allergy= Anagram.new('allergy')
+
+  end
+end
